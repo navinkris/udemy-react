aa8c388a0e379d9f88efc461ccac292ec0c1bddc
navinkris <navin.krishnamurthi@nokia.com>
2023-07-26 14:26:47 +0530

    **Jira:** NSDWAN-1
    **Description:**
    - Added code to provide new error state
    - Handled the error in the UI.
    
    **Testing:**
    - Verified the error getting displayed in case of failure
    
    **Notes:**
    - Additional notes

--- food-project/src/components/Meals/AvailableMeals.js
+++ food-project/src/components/Meals/AvailableMeals.js
@@ -7,10 +7,18 @@ import MealItem from './MealItem/MealItem';
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
 
   useEffect(() => {
     const fetchMeals = async () => {
-      const response = await fetch('https://react-http-ec4f0-default-rtdb.firebaseio.com/meals.json');
+      const response = await fetch(
+        'https://react-http-ec4f0-default-rtdb.firebaseio.com/meals.json'
+        );
+
+        if(!response.ok) {
+          throw new Error('Something went wrong!');
+        }
+
       const responseData = await response.json();
     
       const loadedMeals = [];
@@ -28,7 +36,11 @@ const AvailableMeals = () => {
       setIsLoading(false);
     };
 
-    fetchMeals();
+   fetchMeals().catch((error) => {
+    setIsLoading(false);
+    setHttpError(error.message);
+   });
+    
   }, []);
 
   if(isLoading) {
@@ -37,6 +49,14 @@ const AvailableMeals = () => {
         <p>Loading...</p>
       </section>
     );
+  }
+
+  if(httpError) {
+    return (
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
+      </section>
+    )
   }
 
   const mealsList = meals.map(meal => 
