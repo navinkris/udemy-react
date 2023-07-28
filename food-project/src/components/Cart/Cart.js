e628c88d5f0eb247d5fd240280fcb87faaaea31d
navinkris <navin.krishnamurthi@nokia.com>
2023-07-28 12:39:23 +0530

    **Jira:** NSDWAN-1
    **Description:**
    - Added submit handler
    
    **Testing:**
    - Verified the submited value in the firebase
    
    **Notes:**
    - Additional notes

--- food-project/src/components/Cart/Cart.js
+++ food-project/src/components/Cart/Cart.js
@@ -24,6 +24,16 @@ const Cart = (props) => {
     setIsCheckout(true);
   };
 
+  const submitOrderHandler = (userData) => {
+    fetch('https://react-http-ec4f0-default-rtdb.firebaseio.com/orders.json', {
+      method: 'POST',
+      body: JSON.stringify({
+        user: userData,
+        orderedItem: cartCtx.items,
+      })
+    })
+  };
+
   const cartItems = (
     <ul className={classes['cart-items']}>
     {cartCtx.items.map((item) => (
@@ -53,7 +63,7 @@ const Cart = (props) => {
         <span>Total Amount</span>
         <span>{totalAmount}</span>
       </div>
-      {isCheckout && <Checkout onCancel={props.onClose} />}
+      {isCheckout && <Checkout onConfirm={submitOrderHandler} onCancel={props.onClose} />}
       {!isCheckout && modalActions}
     </Modal>
   );
